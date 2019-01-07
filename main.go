f8ab764307b093e7e28ed6aae99c7795a8a90b80
MaMe82 <mame8282@googlemail.com>
2019-01-07 22:01:46 +0100

    Started work on dongle enumeration calls

--- main.go
+++ main.go
@@ -7,11 +7,12 @@ import (
 	"github.com/google/gousb"
 	"os"
 	"os/signal"
+	"time"
 )
 
 var (
-	eNoDongle = errors.New("No Unifying dongle found")
-	eNoHidPPMsg = errors.New("No valid HID++ 1.0 report")
+	eNoDongle        = errors.New("No Unifying dongle found")
+	eNoHidPPMsg      = errors.New("No valid HID++ 1.0 report")
 	eNoHidPPReportID = errors.New("No valid HID++ 1.0 report type (set to HIDPP_TYPE_SHORT or HIDPP_TYPE_LONG)")
 )
 
@@ -21,22 +22,23 @@ const (
 )
 
 type HidPPReportType byte
+
 const (
 	HIDPP_TYPE_SHORT HidPPReportType = 0x10
-	HIDPP_TYPE_LONG HidPPReportType = 0x11
+	HIDPP_TYPE_LONG  HidPPReportType = 0x11
 )
 const (
-	HIDPP_TYPE_SHORT_LEN = 7
-	HIDPP_TYPE_LONG_LEN = 20
-	HIDPP_TYPE_SHORT_PAYLOAD_LEN = 3
-	HIDPP_TYPE_LONG_PAYLOAD_LEN = 16
+	HIDPP_TYPE_SHORT_LEN         = 7
+	HIDPP_TYPE_LONG_LEN          = 20
+	HIDPP_TYPE_SHORT_PAYLOAD_LEN = 4
+	HIDPP_TYPE_LONG_PAYLOAD_LEN  = 17
 )
 
 const (
-	UNIFYING_MSG_ID_SET_REGISTER_REQ = 0x80
-	UNIFYING_MSG_ID_SET_REGISTER_RSP = 0x80
-	UNIFYING_MSG_ID_GET_REGISTER_REQ = 0x81
-	UNIFYING_MSG_ID_GET_REGISTER_RSP = 0x81
+	UNIFYING_MSG_ID_SET_REGISTER_REQ      = 0x80
+	UNIFYING_MSG_ID_SET_REGISTER_RSP      = 0x80
+	UNIFYING_MSG_ID_GET_REGISTER_REQ      = 0x81
+	UNIFYING_MSG_ID_GET_REGISTER_RSP      = 0x81
 	UNIFYING_MSG_ID_SET_LONG_REGISTER_REQ = 0x82
 	UNIFYING_MSG_ID_SET_LONG_REGISTER_RSP = 0x82
 	UNIFYING_MSG_ID_GET_LONG_REGISTER_REQ = 0x83
@@ -47,10 +49,11 @@ const (
 
 const (
 	UNIFYING_REGISTER_WIRELESS_NOTIFICATIONS = 0x00
-	UNIFYING_REGISTER_CONNECTION_STATE = 0x02
-	UNIFYING_REGISTER_PAIRING = 0xb2
-	UNIFYING_REGISTER_DEVICE_ACTIVITY = 0xb3
-	UNIFYING_REGISTER_PAIRING_INFORMATION = 0xb5
+	UNIFYING_REGISTER_CONNECTION_STATE       = 0x02
+	UNIFYING_REGISTER_PAIRING                = 0xb2
+	UNIFYING_REGISTER_DEVICE_ACTIVITY        = 0xb3
+	UNIFYING_REGISTER_PAIRING_INFORMATION    = 0xb5
+	UNIFYING_REGISTER_FIRMWARE    = 0xf1
 )
 
 // for write/read parameters of short read/write from/to wireless notification register (0x00)
@@ -58,8 +61,7 @@ const (
 	UNIYING_WIRELESS_NOTIFICATIONS_P0_BATTERY_STATUS_MASK = (1 << 4)
 
 	UNIYING_WIRELESS_NOTIFICATIONS_P1_WIRELESS_NOTIFICATIONS_MASK = (1 << 0)
-	UNIYING_WIRELESS_NOTIFICATIONS_P1_SOFTWARE_PRESENT_MASK = (1 << 3)
-
+	UNIYING_WIRELESS_NOTIFICATIONS_P1_SOFTWARE_PRESENT_MASK       = (1 << 3)
 )
 
 type HidPPMsg struct {
@@ -78,7 +80,7 @@ func (r *HidPPMsg) FromWire(payload []byte) (err error) {
 		r.ReportID = HIDPP_TYPE_LONG
 		r.DeviceID = payload[1]
 		r.MsgSubID = payload[2]
-		r.Parameters = make([]byte,16)
+		r.Parameters = make([]byte, HIDPP_TYPE_LONG_PAYLOAD_LEN)
 		copy(r.Parameters, payload[3:])
 		return
 	}
@@ -86,7 +88,7 @@ func (r *HidPPMsg) FromWire(payload []byte) (err error) {
 		r.ReportID = HIDPP_TYPE_SHORT
 		r.DeviceID = payload[1]
 		r.MsgSubID = payload[2]
-		r.Parameters = make([]byte,3)
+		r.Parameters = make([]byte, HIDPP_TYPE_SHORT_PAYLOAD_LEN)
 		copy(r.Parameters, payload[3:])
 		return
 	}
@@ -103,7 +105,7 @@ func (r *HidPPMsg) ToWire() (payload []byte, err error) {
 		if r.Parameters != nil {
 			copy(payload[3:], r.Parameters)
 		}
-		return payload,nil
+		return payload, nil
 	}
 
 	if r.ReportID == HIDPP_TYPE_LONG {
@@ -114,7 +116,7 @@ func (r *HidPPMsg) ToWire() (payload []byte, err error) {
 		if r.Parameters != nil {
 			copy(payload[3:], r.Parameters)
 		}
-		return payload,nil
+		return payload, nil
 	}
 
 	err = eNoHidPPReportID
@@ -122,82 +124,91 @@ func (r *HidPPMsg) ToWire() (payload []byte, err error) {
 }
 
 type Unifying struct {
-	UsbCtx *gousb.Context
-	Dev *gousb.Device
-	Config *gousb.Config
+	UsbCtx     *gousb.Context
+	Dev        *gousb.Device
+	Config     *gousb.Config
 	IfaceHIDPP *gousb.Interface
-	EpInHidPP *gousb.InEndpoint
+	EpInHidPP  *gousb.InEndpoint
 
-	sndQueue chan []byte
-	rcvQueue chan []byte
-	cancel context.CancelFunc
-	ctx context.Context
-
-	reqRespQueue chan [2]*HidPPMsg
+	sndQueue chan *HidPPMsg
+	rcvQueue chan *HidPPMsg
+	cancel   context.CancelFunc
+	ctx      context.Context
 }
 
-func (u *Unifying) SendHidPPMessage(msg HidPPMsg) (err error) {
-	p,err := msg.ToWire()
-	if err != nil {
-		return err
-	}
-	u.sndQueue <- p
+func (u *Unifying) SendHidPPMessage(msg *HidPPMsg) (err error) {
+	u.sndQueue <- msg
 	return nil
 }
 
-/*
-func (u *Unifying) SendRcvHidPPMessage(msg HidPPMsg, timeout time.Duration) (res *HidPPMsg, err error) {
-	p,err := msg.ToWire()
-	if err != nil {
-		return nil,err
+func (u *Unifying) RcvHidPPMessage(timeoutMillis int) (msg *HidPPMsg, err error) {
+	ctx := context.Background()
+	if timeoutMillis > 0 {
+		ctxNew, cancel := context.WithTimeout(ctx, time.Duration(timeoutMillis)*time.Millisecond)
+		defer cancel()
+		ctx = ctxNew
 	}
-	u.sndQueue <- p
 
-	return nil
+	select {
+	case rcv := <-u.rcvQueue:
+		msg = rcv
+	case <-ctx.Done():
+		err = errors.New("timeout reached")
+	}
+
+	return
 }
-*/
 
 func (u *Unifying) readLoop() {
 	buf := make([]byte, 32)
 
 	for {
-		n,err := u.EpInHidPP.ReadContext(u.ctx, buf)
+		n, err := u.EpInHidPP.ReadContext(u.ctx, buf)
 		if err != nil {
 			break
 		}
-		rcv := make([]byte, n)
-		copy(rcv, buf[:n])
-		u.rcvQueue <- rcv
+
+		inMsg := HidPPMsg{}
+		parseErr := inMsg.FromWire(buf[:n])
+		if parseErr == nil {
+			fmt.Printf("In: % #x\n", buf[:n])
+			u.rcvQueue <- &inMsg
+		} else {
+			fmt.Printf("USB input report isn't HID++: % #x\n", buf[:n])
+		}
 	}
 
 	close(u.rcvQueue)
 }
 
 func (u *Unifying) sndRcvLoop() {
-	Outer:
+Outer:
 	for {
 		select {
-		case <- u.ctx.Done():
+		case <-u.ctx.Done():
 			break Outer
-		case outdata := <- u.sndQueue:
-			if len(outdata) == 0 {
-				continue
+		case outMsg := <-u.sndQueue:
+			outdata, err := outMsg.ToWire()
+			if err != nil {
+				fmt.Println("Error processing outbound HID++ message", err)
 			}
+
 			fmt.Printf("Out: % #x\n", outdata)
 			u.Dev.Control(
-				0x21, //bit7: Host to device, bit6..5: Class: 0x1, bit4..0: Interface: 0x01
-				0x09, //request: 0x09 SET_REPORT
-				0x0200 | uint16(outdata[0]), //Output: 0x02, Report ID: 0x10
-				2, //Index 0x02
-				outdata, //payload
+				0x21,                      //bit7: Host to device, bit6..5: Class: 0x1, bit4..0: Interface: 0x01
+				0x09,                      //request: 0x09 SET_REPORT
+				0x0200|uint16(outdata[0]), //Output: 0x02, Report ID: 0x10
+				2,                         //Index 0x02
+				outdata,                   //payload
 			)
-		case indata := <- u.rcvQueue:
-			fmt.Printf("In: % #x\n", indata)
+			/*
+			case indata := <- u.rcvQueue:
+				fmt.Printf("In: % #x\n", indata)
+			*/
 		}
 	}
 
 	close(u.sndQueue)
-	close(u.reqRespQueue)
 }
 
 func (u *Unifying) Close() {
@@ -229,7 +240,7 @@ func NewUnifying() (res *Unifying, err error) {
 	res = &Unifying{}
 
 	res.UsbCtx = gousb.NewContext()
-	res.Dev,err = res.UsbCtx.OpenDeviceWithVIDPID(UNIFYING_VID, UNIFYING_PID)
+	res.Dev, err = res.UsbCtx.OpenDeviceWithVIDPID(UNIFYING_VID, UNIFYING_PID)
 	if err != nil || res.Dev == nil {
 		res.Close()
 		return nil, eNoDongle
@@ -237,10 +248,10 @@ func NewUnifying() (res *Unifying, err error) {
 	fmt.Println("Unifying dongle found", res.Dev)
 
 	//Get device config 1
-	res.Config,err = res.Dev.Config(1)
+	res.Config, err = res.Dev.Config(1)
 	if err != nil {
 		res.Close()
-		return nil,errors.New("Couldn't retrieve config 1 of Unifying dongle")
+		return nil, errors.New("Couldn't retrieve config 1 of Unifying dongle")
 	}
 
 	fmt.Println("Using dongle USB config:", res.Config.Desc.String())
@@ -249,15 +260,15 @@ func NewUnifying() (res *Unifying, err error) {
 	//res.Dev.Reset()
 	res.Dev.SetAutoDetach(true)
 
-	Outer:
-	for _,ifaceDesc := range res.Config.Desc.Interfaces {
-		for _,ifaceSettings := range ifaceDesc.AltSettings {
+Outer:
+	for _, ifaceDesc := range res.Config.Desc.Interfaces {
+		for _, ifaceSettings := range ifaceDesc.AltSettings {
 			//fmt.Printf("%+v\n", ifaceSettings.Endpoints)
 			for _, epDesc := range ifaceSettings.Endpoints {
 				if epDesc.MaxPacketSize == 32 && epDesc.Direction == gousb.EndpointDirectionIn {
 					// This is the HID++ EP
 					//fmt.Printf("EP %+v\n", epDesc.Number)
-					res.IfaceHIDPP,err = res.Config.Interface(ifaceSettings.Number, ifaceSettings.Alternate)
+					res.IfaceHIDPP, err = res.Config.Interface(ifaceSettings.Number, ifaceSettings.Alternate)
 					if err != nil {
 						res.Close()
 						return nil, errors.New("Couldn't access HID++ USB interface")
@@ -265,7 +276,7 @@ func NewUnifying() (res *Unifying, err error) {
 						fmt.Println("HID++ interface:", res.IfaceHIDPP.String())
 					}
 
-					res.EpInHidPP,err = res.IfaceHIDPP.InEndpoint(epDesc.Number)
+					res.EpInHidPP, err = res.IfaceHIDPP.InEndpoint(epDesc.Number)
 					if err != nil {
 						res.Close()
 						return nil, errors.New("Couldn't access HID++ USB interface IN endpoint")
@@ -280,15 +291,13 @@ func NewUnifying() (res *Unifying, err error) {
 
 	if res.EpInHidPP == nil {
 		res.Close()
-		return nil,errors.New("Couldn't find EP for HID++ input reports")
+		return nil, errors.New("Couldn't find EP for HID++ input reports")
 	}
 
-	res.reqRespQueue = make(chan [2]*HidPPMsg)
+	res.sndQueue = make(chan *HidPPMsg)
+	res.rcvQueue = make(chan *HidPPMsg)
 
-	res.sndQueue = make(chan []byte)
-	res.rcvQueue = make(chan []byte)
-
-	res.ctx,res.cancel = context.WithCancel(context.Background())
+	res.ctx, res.cancel = context.WithCancel(context.Background())
 
 	go res.readLoop()
 	go res.sndRcvLoop()
@@ -296,22 +305,129 @@ func NewUnifying() (res *Unifying, err error) {
 	return
 }
 
-
 func main() {
-	u,err := NewUnifying()
+	u, err := NewUnifying()
 	if err != nil {
 		panic(err)
 	}
 	defer u.Close()
 
-	u.SendHidPPMessage(HidPPMsg{
-		ReportID: HIDPP_TYPE_SHORT,
-		DeviceID: 0xff,
-		MsgSubID: UNIFYING_MSG_ID_GET_LONG_REGISTER_REQ,
-		Parameters: []byte{0xb5, 0x03},
-	})
+	// Request receiver data from pairing information register, p1 = 0x03 (undocumented)
+	req := &HidPPMsg{
+		ReportID:   HIDPP_TYPE_SHORT,
+		DeviceID:   0xff,
+		MsgSubID:   UNIFYING_MSG_ID_GET_LONG_REGISTER_REQ,
+		Parameters: []byte{UNIFYING_REGISTER_PAIRING_INFORMATION, 0x03}, //Param 03, no pairing info, but info on receiver itself
+	}
+	u.SendHidPPMessage(req)
+
+	rsp, err := u.RcvHidPPMessage(500)
+	//fmt.Printf("%+v\n", rsp)
+	if err == nil &&
+		rsp.MsgSubID == UNIFYING_MSG_ID_GET_LONG_REGISTER_RSP &&
+		req.Parameters[0] == UNIFYING_REGISTER_PAIRING_INFORMATION &&
+		req.Parameters[1] == 0x03 {
+			dongleAddr := rsp.Parameters[2:6]
+			unknown1 := rsp.Parameters[6] //Proto ??? 0x04 == Unifying
+			unknown2 := rsp.Parameters[7] //max devices ??? 0x06
+			unknownRest := rsp.Parameters[8:]
+		fmt.Printf("Dongle address: % #x, unknown data1: %#x, unknown data2: %#x, unknown rest: % #x\n", dongleAddr, unknown1, unknown2, unknownRest)
+	} else {
+		fmt.Printf("Wrong resp1: %+v\n", rsp)
+	}
+
+	// Request major/minor dongle level of firmware from firmware register
+	req = &HidPPMsg{
+		ReportID:   HIDPP_TYPE_SHORT,
+		DeviceID:   0xff,
+		MsgSubID:   UNIFYING_MSG_ID_GET_REGISTER_REQ,
+		Parameters: []byte{UNIFYING_REGISTER_FIRMWARE, 0x01}, //Param 03, no pairing info, but info on receiver itself
+	}
+	u.SendHidPPMessage(req)
+
+	rsp, err = u.RcvHidPPMessage(500)
+	if err == nil &&
+		rsp.MsgSubID == UNIFYING_MSG_ID_GET_REGISTER_RSP &&
+		req.Parameters[0] == UNIFYING_REGISTER_FIRMWARE &&
+		req.Parameters[1] == 0x01 {
+			major := rsp.Parameters[2]
+			minor := rsp.Parameters[3]
+		fmt.Printf("Firmware maj.min: %.2x.%.2x\n", major, minor)
+	} else {
+		fmt.Printf("Wrong resp2: %+v\n", rsp)
+	}
+
+	// Request dongle firmware patch version from firmware register
+	req = &HidPPMsg{
+		ReportID:   HIDPP_TYPE_SHORT,
+		DeviceID:   0xff,
+		MsgSubID:   UNIFYING_MSG_ID_GET_REGISTER_REQ,
+		Parameters: []byte{UNIFYING_REGISTER_FIRMWARE, 0x02}, //Param 03, no pairing info, but info on receiver itself
+	}
+	u.SendHidPPMessage(req)
+
+	rsp, err = u.RcvHidPPMessage(500)
+	if err == nil &&
+		rsp.MsgSubID == UNIFYING_MSG_ID_GET_REGISTER_RSP &&
+		req.Parameters[0] == UNIFYING_REGISTER_FIRMWARE &&
+		req.Parameters[1] == 0x02 {
+			patchMSB := rsp.Parameters[2]
+			patchLSB := rsp.Parameters[3]
+		fmt.Printf("Firmware patch: %.2x%.2x\n", patchMSB, patchLSB)
+	} else {
+		fmt.Printf("Wrong resp3: %+v\n", rsp)
+	}
+
+	// Request dongle bootloader patch version from firmware register
+	req = &HidPPMsg{
+		ReportID:   HIDPP_TYPE_SHORT,
+		DeviceID:   0xff,
+		MsgSubID:   UNIFYING_MSG_ID_GET_REGISTER_REQ,
+		Parameters: []byte{UNIFYING_REGISTER_FIRMWARE, 0x04}, //Param 03, no pairing info, but info on receiver itself
+	}
+	u.SendHidPPMessage(req)
+
+	rsp, err = u.RcvHidPPMessage(500)
+	if err == nil &&
+		rsp.MsgSubID == UNIFYING_MSG_ID_GET_REGISTER_RSP &&
+		req.Parameters[0] == UNIFYING_REGISTER_FIRMWARE &&
+		req.Parameters[1] == 0x04 {
+		major := rsp.Parameters[2]
+		minor := rsp.Parameters[3]
+		fmt.Printf("Bootloader maj.min.patch: %.2x.%.2x\n", major, minor)
+	} else {
+		fmt.Printf("Wrong resp4: %+v\n", rsp)
+	}
 
 
+	//Enable wireless notifications (to be able to receive infos via device connect notify on new devices)
+	req = &HidPPMsg{
+		ReportID:   HIDPP_TYPE_SHORT,
+		DeviceID:   0xff,
+		MsgSubID:   UNIFYING_MSG_ID_SET_REGISTER_REQ,
+		Parameters: []byte{UNIFYING_REGISTER_WIRELESS_NOTIFICATIONS, 0x00, 0x01, 0x00},
+	}
+	u.SendHidPPMessage(req)
+
+	rsp, err = u.RcvHidPPMessage(500)
+	if err == nil &&
+		rsp.MsgSubID == UNIFYING_MSG_ID_SET_REGISTER_RSP &&
+		req.Parameters[0] == UNIFYING_REGISTER_WIRELESS_NOTIFICATIONS &&
+		req.Parameters[1] == 0x00 {
+		fmt.Printf("Wireless notifications enabled\n")
+	} else {
+		fmt.Printf("Wrong resp4: %+v\n", rsp)
+	}
+
+
+
+
+	go func() {
+		for {
+			fmt.Println(u.RcvHidPPMessage(0))
+		}
+
+	}()
 
 	//Catch signal
 	c := make(chan os.Signal, 1)
@@ -323,6 +439,5 @@ func main() {
 		os.Exit(0)
 		return
 	}
-
 
 }
